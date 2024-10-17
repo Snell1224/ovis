338c1dcedc5fc7fb6cede866475d322f1a03aa51
Sara Walton <spwalto@sandia.gov>
2024-10-17 15:33:01 -0600

    trying python autodoc

--- rtd/source/conf.py
+++ rtd/source/conf.py
@@ -1,5 +1,22 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+from sphinx.ext import apidoc
+
+# Path setup (add project root to sys.path)
+sys.path.insert(0, os.path.abspath('../../ovis'))
+
+# Dynamically generate .rst files from your package
+def run_apidoc(_):
+    """Generate .rst files dynamically from the 'ovis' package."""
+    ignore_paths = ['rtd/source/conf.py']
+    apidoc.main(['-f', '-o', 'rtd/source', 'ovis', *ignore_paths])
+
+# Connect the function to Sphinx's event
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
 # -- Project information
 
 project = 'OVIS-HPC'
@@ -24,11 +41,11 @@ intersphinx_mapping = {
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 
     # Link to the "apis" of the "hpc-ovis" project and subprojects
-    "ovis-hpc": ("https://ovis-hpc.readthedocs.io/en/latest/", None),
-    "sos": ("https://ovis-hpc.readthedocs.io/projects/sos/en/latest/", None),
-    "maestro": ("https://ovis-hpc.readthedocs.io/projects/maestro/en/latest/", None),
-    "baler": ("https://ovis-hpc.readthedocs.io/projects/baler/en/latest/", None),
-    "ldms": ("https://ovis-hpc.readthedocs.io/projects/ldms/en/latest/", None),
+    "ovis-hpc": ("https://ovis-hpc-personal.readthedocs.io/en/latest/", None),
+    "sos": ("https://ovis-hpc-personal.readthedocs.io/projects/sos/en/latest/", None),
+    "maestro": ("https://ovis-hpc-personal.readthedocs.io/projects/maestro/en/latest/", None),
+    "baler": ("https://ovis-hpc-personal.readthedocs.io/projects/baler/en/latest/", None),
+    "ldms": ("https://ovis-hpc-personal.readthedocs.io/projects/ldms/en/latest/", None),
 
 }
 intersphinx_disabled_domains = ['std']
@@ -40,7 +57,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
-html_logo = "https://github.com/ovis-hpc/readthedocs/blob/main/docs/source/images/ovis-logo.png?raw=true"
+html_logo = "https://github.com/ovis-hpc-personal/readthedocs/blob/main/docs/source/images/ovis-logo.png?raw=true"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
